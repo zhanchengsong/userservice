ccf139bfe1b462d3dc46c5f790744532c19448a2
Zhancheng Song <zhanchengsong@Zhanchengs-iMac.local>
2020-06-16 01:59:05 -0400

    Fixed http testing code

--- route/routes.go
+++ route/routes.go
@@ -1,12 +1,16 @@
 package route
+
 import (
-	"github.com/gorilla/mux"
-	"github.com/zhanchengsong/userservice/controllers"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/zhanchengsong/userservice/controllers"
 )
+
 func Handlers() *mux.Router {
 	r := mux.NewRouter().StrictSlash(true)
+	r.Use(LoggingMiddleware)
 	r.Use(CommonMiddleware)
 	// These are the
 	r.HandleFunc("/user", controllers.CreateUser).Methods("POST")
@@ -24,6 +28,13 @@ func CommonMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Received request at", r.URL, r.Method)
 		next.ServeHTTP(w, r)
 	})
 }
--- service.go
+++ service.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
-	"github.com/zhanchengsong/userservice/route"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/zhanchengsong/userservice/route"
 )
+
 // This service handle user related request
 // Incliding CRUD on user, friend/unfriend
 func main() {
 	// Loading extra env settings from .env file
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Cannot not load env file")
+		log.Println("Cannot not load env file")
 	}
 	http.Handle("/", route.Handlers())
 	// Start service
-	port := "3001"
+	port := os.Getenv("SERVER_PORT")
 	log.Printf("Server up on port '%s'", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
