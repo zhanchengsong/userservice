90147236741432f7d96855833d7bdfc0472af205
zhancheng <zhanchengdev@gmail.com>
2020-09-03 01:09:14 -0400

    Restoring the unique_index

--- model/user.go
+++ model/user.go
@@ -5,11 +5,11 @@ import (
 )
 
 type User struct {
-	gorm.Model           `json:-`
-	DisplayName string   `json:"displayName,omitempty"`
-	Username    string   `json:"username",gorm:"type:varchar(100);unique_index"`
-	Email       string   `json:"email",gorm:"type:varchar(100);unique_index"`
-	Password    string   `json:password`
-	IconUrl    	string   `json:"iconUrl,omitempty"`
-	JWTToken    string 	 `json:"jwtToken,omitempty"`
+	gorm.Model  `json:"-"`
+	DisplayName string `json:"displayName,omitempty"`
+	Username    string `json:"username" gorm:"type:varchar(100);unique_index"`
+	Email       string `json:"email" gorm:"type:varchar(100);unique_index"`
+	Password    string `json:"password"`
+	IconUrl     string `json:"iconUrl,omitempty"`
+	JWTToken    string `json:"jwtToken,omitempty"`
 }
