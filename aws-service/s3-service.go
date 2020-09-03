6789865e963dddc973ef9cafa0bb8f39e4b967aa
zhancheng <zhanchengdev@gmail.com>
2020-09-03 01:10:35 -0400

    make s3 servce return a error object instead of HttpError

--- aws-service/s3-service.go
+++ aws-service/s3-service.go
@@ -6,31 +6,30 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/joho/godotenv"
-	"github.com/zhanchengsong/userservice/utils"
 	"io"
 	"os"
 )
 
-func UploadIconToS3(f io.Reader, filename string) (downloadPath string, error *utils.HttpError) {
+func UploadIconToS3(f io.Reader, filename string) (downloadPath string, error error) {
 	// For testing only
 	godotenv.Load()
-	accessKeyId:= os.Getenv("AWS_ACCESS_KEY_ID")
+	accessKeyId := os.Getenv("AWS_ACCESS_KEY_ID")
 	accessKeySecret := os.Getenv("AWS_SECRET_ACCESS_KEY")
 	bucketName := os.Getenv("AWS_USER_ICON_BUCKET")
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String(os.Getenv("AWS_REGION")),
 		Credentials: credentials.NewStaticCredentials(accessKeyId,
-													  accessKeySecret,
-													  ""),
+			accessKeySecret,
+			""),
 	}))
 	uploader := s3manager.NewUploader(sess)
 	result, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucketName),
-		Key: aws.String(filename),
-		Body: f,
+		Key:    aws.String(filename),
+		Body:   f,
 	})
 	if err != nil {
-		return "", &utils.HttpError{Err: err.Error()}
+		return "", err
 	}
 	return result.Location, nil
 }
